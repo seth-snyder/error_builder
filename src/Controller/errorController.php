3a2a30cc81193b5091f86c1bf71ade95f365422b
Seth Snyder <seth@sethsnyder.com>
2022-02-22 19:21:55 +0000

    Initial Commit

---
+++ src/Controller/errorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\error_builder\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for TFA SendGrid Reports routes.
+ */
+class errorController extends ControllerBase {
+
+  /**
+   * Bounces by Date Summary Report.
+   * Version 0
+   */
+  public function shoutOut() {
+     $build['header'] = [
+      '#type' => 'item',
+      '#markup' => '<h1>' . 'yo' . '</h1>',
+    ];
+    return $build;
+  }
+
+}
